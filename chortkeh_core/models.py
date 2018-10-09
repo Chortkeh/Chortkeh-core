7cc95c6bc47b3e727e0e6310052cdaa05362314c
Rasool Safari <safari@rasooll.com>
2018-10-09 17:50:36 +0330

    Create models

--- chortkeh_core/models.py
+++ chortkeh_core/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Wallet(models.Model):
+    """ This model for wallets """
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    balance = models.BigIntegerField()
+
+
+class Group(models.Model):
+    """ This model use for group of income and expense. """
+
+    action_type_choices = (
+        ('inc', 'Income'), ('exp', 'Expense'), ('und', 'undefined'),
+    )
+    action_type = models.CharField(
+        max_length=3, choices=action_type_choices, default='und')
+    owner = models.ForeignKey(
+        User, on_delete=models.CASCADE, blank=True, null=True)
+    default = models.BooleanField(default=False)
+    name = models.CharField(max_length=255)
+
+
+class Income(models.Model):
+    """ This model for income transactions. """
+
+    amount = models.BigIntegerField()
+    time = models.DateTimeField()
+    comment = models.CharField(max_length=255, blank=True, null=True)
+    Wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL)
+
+
+class Expense(models.Model):
+    """ This model for expense transactions. """
+
+    amount = models.BigIntegerField()
+    time = models.DateTimeField()
+    comment = models.CharField(max_length=255, blank=True, null=True)
+    Wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL)
