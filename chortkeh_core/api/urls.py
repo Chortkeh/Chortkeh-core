a60dc743e9fa98358cb44f2c78ff815a2384acff
Rasool Safari <safari@rasooll.com>
2018-10-10 13:10:48 +0330

    Change urls

--- chortkeh_core/api/urls.py
+++ chortkeh_core/api/urls.py
@@ -1,4 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from .views.account import GetToken, Account
 
+# Account urls #
+account = [
+    path('', Account.as_view(), name='account'),
+    path('get_token/', GetToken.as_view(), name='get_token'),
+]
+
+# Main urls #
 urlpatterns = [
+    path('account/', include(account)),
 ]
