9d9623f20bec596c72f09de0e676c62504f044e0
Rasool Safari <safari@rasooll.com>
2018-10-11 19:40:12 +0330

    Fix serializer

--- chortkeh_core/api/serializers.py
+++ chortkeh_core/api/serializers.py
@@ -6,11 +6,13 @@ class CreateAccountSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=150)
     email = serializers.EmailField()
     first_name = serializers.CharField(max_length=30)
-    last_name = serializers.CharField(max_length=150, required=False)
+    last_name = serializers.CharField(
+        max_length=150, allow_blank=True, allow_null=True)
 
 
 class UpdateAccountSerializer(serializers.Serializer):
 
     email = serializers.EmailField()
     first_name = serializers.CharField(max_length=30)
-    last_name = serializers.CharField(max_length=150, required=False)
+    last_name = serializers.CharField(
+        max_length=150, allow_blank=True, allow_null=True)
