fb20b9313b062f7d742469292829e09010fbda0a
Rasool Safari <safari@rasooll.com>
2018-10-13 15:00:17 +0330

    Add Incom transaction serializer

--- chortkeh_core/api/serializers.py
+++ chortkeh_core/api/serializers.py
@@ -28,3 +28,13 @@ class GroupSerializer(serializers.Serializer):
     action_type_choices = (('inc', 'Income'), ('exp', 'Expense'),)
     name = serializers.CharField(max_length=255)
     action_type = serializers.ChoiceField(choices=action_type_choices)
+
+
+class IncomeTransactionSerializer(serializers.Serializer):
+
+    amount = serializers.IntegerField()
+    time = serializers.DateTimeField()
+    comment = serializers.CharField(
+        max_length=255, allow_blank=True, allow_null=True)
+    wallet_id = serializers.IntegerField()
+    group_id = serializers.IntegerField()
