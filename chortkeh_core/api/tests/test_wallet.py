d94e5836428c866ea53fd730a47d70f86e28a068
Rasool Safari <safari@rasooll.com>
2018-10-24 21:34:34 +0330

    Update wallet testcase.

--- chortkeh_core/api/tests/test_wallet.py
+++ chortkeh_core/api/tests/test_wallet.py
@@ -80,3 +80,17 @@ class WalletTestCase(TestCase):
                 'name': 'Edited'
             }), content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_wallet_no_pk(self):
+        response = self.client.delete(reverse('wallet_api_view'))
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_delete_wallet_pk(self):
+        response = self.client.delete(
+            reverse('wallet_api_view_pk', kwargs={'pk': 1}))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_delete_wallet_pk_not_found(self):
+        response = self.client.delete(
+            reverse('wallet_api_view_pk', kwargs={'pk': 10}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
